cdee74569d892b25521b23450c0fa841682b3638
andxu <andxu@microsoft.com>
2017-09-05 11:35:13 +0800

    remove used files

--- src/source-gen/test-node.js
+++
@@ -1,7 +0,0 @@
-const execFile = require('child_process').execFile;
-const child = execFile('node', (error, stdout, stderr) => {
-});
-setTimeout(() => {
-    child.kill("SIGINT");
-}, 100000);
-console.log(child.pid);
