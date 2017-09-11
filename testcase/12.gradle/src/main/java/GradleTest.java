41a96a15bd89952393c31c8a5d08dc8b086cc2ff
andxu <andxu@microsoft.com>
2017-09-11 09:11:46 +0800

    add gradle test scenario

---
+++ testcase/12.gradle/src/main/java/GradleTest.java
@@ -0,0 +1,6 @@
+public class GradleTest {
+    public static void main(String[] args) {
+        System.out.println("This is a sample gradle project");
+        System.out.println("Now print Chinese: 中文");
+    }
+}
