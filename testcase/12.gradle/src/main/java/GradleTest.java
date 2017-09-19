b00cb6949b9ebe9f784d3b534046153080305a33
andxu <andxu@microsoft.com>
2017-09-19 14:10:36 +0800

    update test case

--- testcase/12.gradle/src/main/java/GradleTest.java
+++ testcase/12.gradle/src/main/java/GradleTest.java
@@ -1,6 +1,5 @@
 public class GradleTest {
     public static void main(String[] args) {
         System.out.println("This is a sample gradle project");
-        System.out.println("Now print Chinese: 中文");
     }
 }
