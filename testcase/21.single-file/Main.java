f46a638bbc1f6853d023e480e8621f52ef9575ea
xuzho <xuzho@microsoft.com>
2017-11-28 18:34:51 +0800

    add testcase for single file
    Signed-off-by: xuzho <xuzho@microsoft.com>

---
+++ testcase/21.single-file/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[] args)
+    {
+        String s = "1";
+        char sr = 'c';
+        int t= 1;
+        t += 1;
+        test();
+    }
+
+    private static void test()
+    {
+        int s = 1;
+        System.out.println("test");
+    }
+}
