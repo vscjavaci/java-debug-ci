cf25c0871f65653d0db7132553f971a8faa6c195
Summer <victoriasqq@gmail.com>
2018-04-24 15:58:22 +0800

    fix test case (#42)

--- testcase/19.java9-app/src/main/java/com/microsoft/app/Launcher.java
+++ testcase/19.java9-app/src/main/java/com/microsoft/app/Launcher.java
@@ -4,7 +4,7 @@ import java.nio.file.Paths;
 
 public class Launcher {
     public static void main(String[] args) {
-        System.out.println(Paths.get(".").toAbsolutePath().toString());
+        System.out.println(Paths.get("").toAbsolutePath().toString());
 
         System.out.println(System.getenv("Path"));
     }
